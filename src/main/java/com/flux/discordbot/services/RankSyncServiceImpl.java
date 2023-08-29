247a7ac01d6e39bb9dbf4b023102ec2a68eb75e7
ziue <remig06@hotmail.com>
2023-08-29 08:13:19 +0200

    added the rest of role relations

--- src/main/java/com/flux/discordbot/services/RankSyncServiceImpl.java
+++ src/main/java/com/flux/discordbot/services/RankSyncServiceImpl.java
@@ -19,6 +19,19 @@ public class RankSyncServiceImpl implements RankSyncService {
     static {
         // MAIN SERVER ROLE ID - COMMISSION SERVER ROLE ID
         roleRelationBidiMap.put(1140378574726123620L, 1141049396901986463L); // Data pack Development
+        roleRelationBidiMap.put(1140377996889424034L, 1141049396901986462L); // Plugin Development
+        roleRelationBidiMap.put(1140377990262427649L, 1141049396901986461L); // Client Development
+        roleRelationBidiMap.put(1140377992833544333L, 1141049396901986460L); // Game Development
+        roleRelationBidiMap.put(1140377996386115764L, 1141049396901986459L); // Web & App Design
+        roleRelationBidiMap.put(1140378309813862441L, 1141049396901986458L); // App Development
+        roleRelationBidiMap.put(1140378632586543114L, 1141049396901986457L); // Full-Stack Development
+        roleRelationBidiMap.put(1140378630673936566L, 1141049396901986456L); // Front-End Development
+        roleRelationBidiMap.put(1140378631600873683L, 1141049396872630361L); // Backend Development
+        roleRelationBidiMap.put(1140377994951663726L, 1141049396872630360L); // Model Designer
+        roleRelationBidiMap.put(1140376933222322186L, 1141049396872630359L); // GFX Designer
+        roleRelationBidiMap.put(1140377991621382245L, 1141049396872630358L); // Video Editor
+        roleRelationBidiMap.put(1140378633769324585L, 1141049396872630357L); // 3D Modeller
+        roleRelationBidiMap.put(1140378918520623164L, 1141049396872630356L); // 3D Design
     }
 
     @Override
