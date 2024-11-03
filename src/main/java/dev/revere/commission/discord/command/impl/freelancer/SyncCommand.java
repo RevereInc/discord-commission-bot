1a4d9142d557a50931cb2ae5471abc5083899673
devuxious <remig06@hotmail.com>
2024-11-03 01:07:29 +0100

    feat(sync-command): allow administrators to sync other freelancers

--- src/main/java/dev/revere/commission/discord/command/impl/freelancer/SyncCommand.java
+++ src/main/java/dev/revere/commission/discord/command/impl/freelancer/SyncCommand.java
@@ -1,13 +1,22 @@
 package dev.revere.commission.discord.command.impl.freelancer;
 
 import dev.revere.commission.discord.utility.TonicEmbedBuilder;
+import dev.revere.commission.entities.Commission;
 import dev.revere.commission.repository.FreelancerRepository;
 import dev.revere.commission.services.RankSyncService;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 @Service
 public class SyncCommand extends SlashCommand {
@@ -22,24 +31,40 @@ public class SyncCommand extends SlashCommand {
         this.name = "sync";
         this.help = "Sync roles for freelancers";
         this.guildOnly = true;
+
+        ArrayList<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.USER, "user", "The freelancer to sync roles for", false));
+        this.options = options;
     }
 
     @Override
     protected void execute(final SlashCommandEvent p_slashCommandEvent) {
-        final Member member = p_slashCommandEvent.getMember();
+        final User user = p_slashCommandEvent.getOption("user") == null
+                ? p_slashCommandEvent.getUser()
+                : p_slashCommandEvent.getOption("user").getAsUser();
 
-        assert member != null;
-        if (!m_freelancerRepository.existsFreelancerByUserId(member.getIdLong())) {
-            p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("You are not currently a freelancer! Feel free to apply.")).setEphemeral(true).queue();
+        if (user != p_slashCommandEvent.getUser() && !Objects.requireNonNull(p_slashCommandEvent.getMember()).hasPermission(Permission.ADMINISTRATOR)) {
+            p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("You are missing the `ADMINISTRATOR` permission required to execute this command.")).setEphemeral(true).queue();
             return;
         }
 
-        if(m_freelancerRepository.findFreelancerByUserId(member.getIdLong()).getDepartments() == null) {
+        if (!m_freelancerRepository.existsFreelancerByUserId(user.getIdLong())) {
+            if (user == p_slashCommandEvent.getUser()) {
+                p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("You are not currently a freelancer! Feel free to apply.")).setEphemeral(true).queue();
+            } else {
+                p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("Could not find freelancer with name " + user.getName())).queue();
+            }
+            return;
+        }
+
+        if(m_freelancerRepository.findFreelancerByUserId(user.getIdLong()).getDepartments() == null) {
             p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("You are not currently a freelancer! Feel free to apply.")).queue();
             return;
         }
 
-        m_rankSyncService.syncMember(member, m_freelancerRepository.findFreelancerByUserId(member.getIdLong()));
+        Member member = Objects.requireNonNull(p_slashCommandEvent.getGuild()).retrieveMemberById(user.getId()).complete();
+
+        m_rankSyncService.syncMember(member, m_freelancerRepository.findFreelancerByUserId(user.getIdLong()));
 
         p_slashCommandEvent.reply(TonicEmbedBuilder.sharedMessageEmbed("Successfully synced your roles in all servers")).setEphemeral(true).queue();
     }
